b3efe50880aeac2c28e818781f4fa6d13578136e
Mehdi <105603194+Ektisad25@users.noreply.github.com>
2024-10-01 21:41:03 +0200

    Update util.py

--- electrumx/lib/util.py
+++ electrumx/lib/util.py
@@ -24,6 +24,8 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # and warranty status of this software.
 
+
+
 '''Miscellaneous utility classes and functions.'''
 
 
@@ -138,8 +140,8 @@ def deep_getsizeof(obj):
 def subclasses(base_class, strict=True):
     '''Return a list of subclasses of base_class in its module.'''
     def select(obj):
-        return (inspect.isclass(obj) and issubclass(obj, base_class) and
-                (not strict or obj != base_class))
+        return (inspect.isclass(obj) and issubclass(obj, base_class)
+                and (not strict or obj != base_class))
 
     pairs = inspect.getmembers(sys.modules[base_class.__module__], select)
     return [pair[1] for pair in pairs]
@@ -346,5 +348,3 @@ def pack_varint(n):
 
 def pack_varbytes(data):
     return pack_varint(len(data)) + data
-
-    
